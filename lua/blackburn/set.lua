39172a2ad308e95915678ac26fd09c3d4ad9b8a5
karlchvojka <karl.chvojka@gmail.com>
2024-08-22 11:54:08 -0400

    IMP: Word Wrap

--- lua/blackburn/set.lua
+++ lua/blackburn/set.lua
@@ -10,7 +10,7 @@ vim.opt.expandtab = true
 
 vim.opt.smartindent = true
 
-vim.opt.wrap = false
+vim.opt.wrap = true
 
 vim.opt.swapfile = false
 vim.opt.backup = false
@@ -29,5 +29,6 @@ vim.opt.isfname:append("@-@")
 vim.opt.updatetime = 50
 
 vim.opt.colorcolumn = "80"
+
 
 
