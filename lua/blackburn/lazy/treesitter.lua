aaac7319b6f676c38ffe306e8aca808c530797ce
karlchvojka <karl.chvojka@protonmail.com>
2024-12-16 17:04:58 -0500

    IMP: Increased Treesitter installed languages to more defaults on install.

--- lua/blackburn/lazy/treesitter.lua
+++ lua/blackburn/lazy/treesitter.lua
@@ -5,7 +5,7 @@ return {
         require("nvim-treesitter.configs").setup({
             -- A list of parser names, or "all"
             ensure_installed = {
-                "vimdoc", "c", "lua", "rust", "bash", "markdown", "markdown_inline",
+                "vimdoc", "c", "lua", "rust", "bash", "markdown", "markdown_inline", "css", "html", "javascript", "jsdoc", "regex", 
             },
 
             -- Install parsers synchronously (only applied to `ensure_installed`)
