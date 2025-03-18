f9aade7c85c79ea95466c1dde88bd90b4671d9bb
karlchvojka <karl.chvojka@gmail.com>
2025-03-18 18:11:23 -0400

    FIX: Parser default listing update
    Updated list of default parsers

--- lua/blackburn/lazy/treesitter.lua
+++ lua/blackburn/lazy/treesitter.lua
@@ -5,7 +5,7 @@ return {
         require("nvim-treesitter.configs").setup({
             -- A list of parser names, or "all"
             ensure_installed = {
-                "vimdoc", "c", "lua", "rust", "bash", "markdown", "markdown_inline", "css", "html", "javascript", "jsdoc", "regex", 
+                "vimdoc", "c", "lua", "bash", "markdown", "markdown_inline", "css", "html", "javascript", "jsdoc", "regex", 
             },
 
             -- Install parsers synchronously (only applied to `ensure_installed`)
