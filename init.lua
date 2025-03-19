6ab661cd785b0c7b4ca9c8bdefdd50fdd827d045
karlchvojka <karl.chvojka@gmail.com>
2025-03-18 23:57:25 -0400

    IMP: Added Nvim-Tree plugin

--- init.lua
+++ init.lua
@@ -7,6 +7,12 @@ Vim Commands
 Note: These run at vim startup
 --]]
 
+-- Disable netrw
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+require("nvim-tree").setup()
+
 -- Runs ColorMyPencils function from: /lua/blackburn/lazy/colors.lua
 vim.cmd "lua ColorMyPencils()"
 
